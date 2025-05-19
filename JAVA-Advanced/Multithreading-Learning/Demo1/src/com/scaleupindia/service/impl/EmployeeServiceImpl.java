f9b0311d314e4373609ea9c09c7104c87ced1a7e
Prashant Kumar <2018187@iiitdmj.ac.in>
2025-05-19 16:09:39 +0530

    Demo1 added

---
+++ JAVA-Advanced/Multithreading-Learning/Demo1/src/com/scaleupindia/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,19 @@
+package com.scaleupindia.service.impl;
+
+import com.scaleupindia.repository.EmployeeRepository;
+
+
+public class EmployeeServiceImpl extends Thread {
+	private EmployeeRepository employeeRepository;
+
+	public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+		super();
+		this.employeeRepository = employeeRepository;
+	}
+
+	public void run() {
+		System.out.println(Thread.currentThread().getName() + " started fetching from : " + employeeRepository);
+		employeeRepository.displayEmployees();
+		System.out.println(Thread.currentThread().getName() + " finished fetching from : " + employeeRepository);
+	}
+}
